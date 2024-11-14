7aad6cb99dc2db678ee988191cb361465b1e2333
caitlinburns <ceburns1@loyola.edu>
2024-11-13 20:13:27 -0500

    code

--- algorithm.md
+++ algorithm.md
@@ -1,2 +1,35 @@
 # Algorithm Document
 #### PLEASE! PLEASE! PLEASE! THINK before you code...
+
+
+* Purpose:  reads filename
+* name: read_filename
+* Parameters: none
+* Return: f_name
+* algorithm:
+  1. 
+
+
+
+* Purpose:  converts file data to a list
+* Name: file_list
+* Parameters: f_name
+* Return: names
+* algorithm:
+  1.  h
+
+
+* Purpose:  prints seat placement
+* Name: seat_placement
+* Parameters: names
+* Return: none
+* algorithm:
+  1.  
+
+
+* Purpose:  runs main program
+* Name: main
+* Parameters: none
+* Return:none
+* algorithm:
+  1.  h
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+
+# Purpose:  reads filename
+# Parameters: none
+# Return: f_name
+def read_filename():
+    f_name = input('Enter file name: ')
+    while not os.path.isfile(f_name):
+        print('Error')
+        f_name = input('Enter file name: ')
+    return f_name
+
+# Purpose:  converts file data to list
+# Parameters: f_name
+# Return: names
+def file_list(f_name):
+    fd = open(f_name, 'r')
+    names = fd.readlines()
+    fd.close()
+    return names
+
+# Purpose: prints seat assignment
+# Parameters: names
+# Return: none
+def seat_placement(names):
+    table = 1
+    seat = 1
+    for name in names:
+
+        print(f'table {table}, seat {seat}, {name}')
+        print('~' * 70)
+        seat += 1
+        if seat == 6:
+            table += 1
+            seat = 1
+
+
+# Purpose: runs main program
+# Parameters: none
+# Return: none
+def main():
+    f_name = read_filename()
+    names = file_list(f_name)
+    seat_placement(names)
+
+
+main()
